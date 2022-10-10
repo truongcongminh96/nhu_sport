b6a4ee0c0ee2b0d5e3eea67091b2d28a987d52ba
s3minhtruong <105472488+s3minhtruong@users.noreply.github.com>
2022-10-10 15:00:48 +0700

    Add laravel breeze authen

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert(
+            [
+                [
+                    'name' => 'Admin',
+                    'username' => 'admin',
+                    'email' => 'admin_email@example.com',
+                    'password' => Hash::make('11111111'),
+                    'status' => 'active'
+                ],
+                [
+                    'name' => 'Main Vendor',
+                    'username' => 'main_vendor',
+                    'email' => 'main_vendor@example.com',
+                    'password' => Hash::make('11111111'),
+                    'status' => 'active'
+                ],
+                [
+                    'name' => 'Test User',
+                    'username' => 'test_user',
+                    'email' => 'test_user@example.com',
+                    'password' => Hash::make('11111111'),
+                    'status' => 'active'
+                ]
+            ]
+        );
+    }
+}
