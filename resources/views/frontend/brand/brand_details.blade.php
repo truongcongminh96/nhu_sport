d484b0f5ef81ef1b05f95d4b58539197951e8b89
Minh Truong <minh.truong@s3corp.com.vn>
2023-03-10 13:51:45 +0700

    Add quick view modal in brand details

--- resources/views/frontend/brand/brand_details.blade.php
+++ resources/views/frontend/brand/brand_details.blade.php
@@ -78,7 +78,7 @@
                                         <a aria-label="Compare" class="action-btn" href="shop-compare.html"><i
                                                 class="fi-rs-shuffle"></i></a>
                                         <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal"
-                                           data-bs-target="#quickViewModal"><i class="fi-rs-eye"></i></a>
+                                           data-bs-target="#quickViewModal" id="{{ $product->id }}" onclick="productView(this.id)"><i class="fi-rs-eye"></i></a>
                                     </div>
                                     @php
                                         $amount = $product->selling_price - $product->discount_price;
