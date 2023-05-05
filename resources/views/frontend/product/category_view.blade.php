925e9e65478b21cec75f95e7639319eb034dcf20
s3minhtruong <minh.truong@s3corp.com.vn>
2023-05-05 13:05:47 +0700

    Update category view

--- resources/views/frontend/product/category_view.blade.php
+++ resources/views/frontend/product/category_view.blade.php
@@ -24,35 +24,17 @@
             <div class="col-lg-4-5">
                 <div class="shop-product-fillter">
                     <div class="totall-product">
-                        <p>We found <strong class="text-brand">{{ count($products) }}</strong> items for you!</p>
+                        <p>Chúng tôi tìm thấy <strong class="text-brand">{{ count($products) }}</strong> sản phẩm cho
+                            bạn!</p>
                     </div>
                     <div class="sort-by-product-area">
-                        <div class="sort-by-cover mr-10">
-                            <div class="sort-by-product-wrap">
-                                <div class="sort-by">
-                                    <span><i class="fi-rs-apps"></i>Show:</span>
-                                </div>
-                                <div class="sort-by-dropdown-wrap">
-                                    <span> 50 <i class="fi-rs-angle-small-down"></i></span>
-                                </div>
-                            </div>
-                            <div class="sort-by-dropdown">
-                                <ul>
-                                    <li><a class="active" href="#">50</a></li>
-                                    <li><a href="#">100</a></li>
-                                    <li><a href="#">150</a></li>
-                                    <li><a href="#">200</a></li>
-                                    <li><a href="#">All</a></li>
-                                </ul>
-                            </div>
-                        </div>
                         <div class="sort-by-cover">
                             <div class="sort-by-product-wrap">
                                 <div class="sort-by">
-                                    <span><i class="fi-rs-apps-sort"></i>Sort by:</span>
+                                    <span><i class="fi-rs-apps-sort"></i>Sắp xếp:</span>
                                 </div>
                                 <div class="sort-by-dropdown-wrap">
-                                    <span> Featured <i class="fi-rs-angle-small-down"></i></span>
+                                    <span> Mặc định <i class="fi-rs-angle-small-down"></i></span>
                                 </div>
                             </div>
                             <div class="sort-by-dropdown">
@@ -80,12 +62,9 @@
                                         </a>
                                     </div>
                                     <div class="product-action-1">
-                                        <a aria-label="Add To Wishlist" class="action-btn" href="shop-wishlist.html"><i
-                                                class="fi-rs-heart"></i></a>
-                                        <a aria-label="Compare" class="action-btn" href="shop-compare.html"><i
-                                                class="fi-rs-shuffle"></i></a>
-                                        <a aria-label="Quick view" class="action-btn" data-bs-toggle="modal"
-                                           data-bs-target="#quickViewModal"><i class="fi-rs-eye"></i></a>
+                                        <a style="width: 100%" aria-label="Quick view" class="btn btn-sm hover-up"
+                                           data-bs-toggle="modal" data-bs-target="#quickViewModal"
+                                           id="{{ $product->id }}" onclick="productView(this.id)">Xem nhanh</a>
                                     </div>
                                     @php
                                         $amount = $product->selling_price - $product->discount_price;
@@ -128,14 +107,15 @@
                                         @else
                                             <div class="product-price">
                                                 <span>{{ number_format($product->discount_price, 0, '') }}</span>
-                                                <span class="old-price">{{ number_format($product->selling_price, 0, '') }}</span>
+                                                <span
+                                                    class="old-price">{{ number_format($product->selling_price, 0, '') }}</span>
                                             </div>
                                         @endif
-                                            <div class="add-cart">
-                                                <a class="add" data-bs-toggle="modal" data-bs-target="#quickViewModal"
-                                                   id="{{ $product->id }}" onclick="productView(this.id)"><i
-                                                        class="fi-rs-shopping-cart mr-5"></i>Add </a>
-                                            </div>
+                                        <div class="add-cart">
+                                            <a class="add" data-bs-toggle="modal" data-bs-target="#quickViewModal"
+                                               id="{{ $product->id }}" onclick="productView(this.id)"><i
+                                                    class="fi-rs-shopping-cart mr-5"></i>Add </a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -156,63 +136,15 @@
                                 $products = App\Models\Product::where('category_id',$category->id)->get();
                             @endphp
                             <li>
-                                <a href="{{ url('product/category/'.$product['category']['id'].'/'.$product['category']['category_slug']) }}"> <img src=" {{ asset($category->category_image) }} "
-                                                                     alt=""/>{{ $category->category_name }}</a><span
-                                    class="count">{{ count($products) }}</span>
+                                <a href="{{ url('product/category/'.$category->id.'/'.$category->category_slug) }}">
+                                    <img src="{{ asset( $category->category_image ) }}"
+                                         alt=""/> {{ $category->category_name }} </a>
+                                <span class="count">{{ count($products) }}</span>
                             </li>
                         @endforeach
                     </ul>
                 </div>
                 <!-- Fillter By Price -->
-                <div class="sidebar-widget price_range range mb-30">
-                    <h5 class="section-title style-1 mb-30">Fill by price</h5>
-                    <div class="price-filter">
-                        <div class="price-filter-inner">
-                            <div id="slider-range" class="mb-20"></div>
-                            <div class="d-flex justify-content-between">
-                                <div class="caption">From: <strong id="slider-range-value1" class="text-brand"></strong>
-                                </div>
-                                <div class="caption">To: <strong id="slider-range-value2" class="text-brand"></strong>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="list-group">
-                        <div class="list-group-item mb-10 mt-10">
-                            <label class="fw-900">Color</label>
-                            <div class="custome-checkbox">
-                                <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox1"
-                                       value=""/>
-                                <label class="form-check-label" for="exampleCheckbox1"><span>Red (56)</span></label>
-                                <br/>
-                                <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox2"
-                                       value=""/>
-                                <label class="form-check-label" for="exampleCheckbox2"><span>Green (78)</span></label>
-                                <br/>
-                                <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox3"
-                                       value=""/>
-                                <label class="form-check-label" for="exampleCheckbox3"><span>Blue (54)</span></label>
-                            </div>
-                            <label class="fw-900 mt-15">Item Condition</label>
-                            <div class="custome-checkbox">
-                                <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox11"
-                                       value=""/>
-                                <label class="form-check-label" for="exampleCheckbox11"><span>New (1506)</span></label>
-                                <br/>
-                                <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox21"
-                                       value=""/>
-                                <label class="form-check-label"
-                                       for="exampleCheckbox21"><span>Refurbished (27)</span></label>
-                                <br/>
-                                <input class="form-check-input" type="checkbox" name="checkbox" id="exampleCheckbox31"
-                                       value=""/>
-                                <label class="form-check-label" for="exampleCheckbox31"><span>Used (45)</span></label>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="shop-grid-right.html" class="btn btn-sm btn-default"><i class="fi-rs-filter mr-5"></i>
-                        Fillter</a>
-                </div>
                 <!-- Product sidebar Widget -->
                 <div class="sidebar-widget product-sidebar mb-30 p-30 bg-grey border-radius-10">
                     <h5 class="section-title style-1 mb-30">{{ $breadCategory->category_name }} MỚI</h5>
@@ -228,29 +160,17 @@
                                 </a>
                             </div>
                             <div class="content pt-10">
-                                <p><a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}">{{ $product->product_name }}</a></p>
+                                <p>
+                                    <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug) }}">{{ $product->product_name }}</a>
+                                </p>
                                 @if($product->discount_price == NULL)
-                                    <p class="price mb-0 mt-5">${{ $product->selling_price }}</p>
+                                    <p class="price mb-0 mt-5">{{ number_format($product->selling_price, 0, '') }}</p>
                                 @else
-                                    <p class="price mb-0 mt-5">${{ $product->discount_price }}</p>
+                                    <p class="price mb-0 mt-5">{{ number_format($product->discount_price, 0, '') }}</p>
                                 @endif
-                                <div class="product-rate">
-                                    <div class="product-rating" style="width: 90%"></div>
-                                </div>
                             </div>
                         </div>
                     @endforeach
-                </div>
-                <div class="banner-img wow fadeIn mb-lg-0 animated d-lg-block d-none">
-                    <img src="{{ asset('frontend/assets/imgs/banner/banner-11.png') }}" alt=""/>
-                    <div class="banner-text">
-                        <span>Oganic</span>
-                        <h4>
-                            Save 17% <br/>
-                            on <span class="text-brand">Oganic</span><br/>
-                            Juice
-                        </h4>
-                    </div>
                 </div>
             </div>
         </div>
